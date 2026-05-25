0a998ba48f3facdf6260052007cf780e2fd14e69
Maksymilian Puchowicz <favlty13@mat.umk.pl>
2026-05-25 08:10:46 +0200

    parse env

--- main_cb.py
+++ main_cb.py
@@ -1,6 +1,10 @@
 
 import discord
 
+DISCORD_TOKEN = None
+APP_ID = None
+PUBLIC_KEY = None
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -10,8 +14,33 @@ class MyClient(discord.Client):
         print(f"Message from {message.author}: {message.content}")
 
 
+def parse_file(path):
+
+    with open(path, "r") as file:
+
+        print(f"parsing {path}...")
+
+        for line in file:
+
+            line = line.strip()
+            if not line:
+                continue
+
+            key, value = line.split("=", 1)
+            value = value.strip("<>")
+
+            globals()[key] = value
+
+            print(f"\tValue: <{value}> added to var: {key}")
+
+    return True
+
+
 intents = discord.Intents.default()
 intents.message_content = True
+parse_file(".env")
 
 client = MyClient(intents=intents)
-client.run('you would like that huh?')
+
+if DISCORD_TOKEN is not None:
+    client.run(DISCORD_TOKEN)
